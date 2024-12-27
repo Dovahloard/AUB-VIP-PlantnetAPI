78eaad0d05017c70a0ca98d758ebf2427b04383b
Dovahloard <57589451+Dovahloard@users.noreply.github.com>
2024-12-27 19:38:57 +0200

    Add files via upload

---
+++ synonymes.py
@@ -0,0 +1,24 @@
+synonymes = {}
+
+synonymes['Alcea setosa'] = ['Synonym']
+synonymes['Allium ampeloprasum'] = ['Synonym']
+synonymes['Allium rotundum'] = ['Synonym']
+synonymes['Anacamptis pyramidalis'] = ['Synonym']
+synonymes['Calicotome villosa'] = ['Synonym']
+synonymes['Centranthus longiflorus'] = ['Synonym']
+synonymes['Cistus creticus'] = ['Synonym']
+synonymes['Crataegus azarolus'] = ['Synonym']
+synonymes['Crepis hierosolymitana'] = ['Synonym']
+synonymes['Hypericum triquetrifolium'] = ['Synonym']
+synonymes['Iris germanica'] = ['Iris hermona','Synonym2']
+synonymes['Linum pubescens'] = ['Synonym']
+synonymes['Orchis anatolica'] = ['Synonym']
+synonymes['Phlomis chrysophylla'] = ['Synonym']
+synonymes['Ranunculus asiaticus'] = ['Synonym']
+synonymes['Rosa canina'] = ['Synonym']
+synonymes['Spartium junceum'] = ['Synonym']
+synonymes['Styrax officinalis'] = ['Synonym']
+
+
+synonymes['Anacamptis pyramidaliss'] = ['Anacamptis pyramidalis','Synonym2']
+synonymes['Alcea Dany'] = ['Alcea setosa']
